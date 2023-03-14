c3e78cb10e48cc61d05e4af56dd0b1771548d22f
muhamadgibran2333 <muhamadgibran2333@gmail.com>
2023-03-14 18:13:09 +0700

    first commit

---
+++ contacts.js
@@ -0,0 +1,9 @@
+const contacts = [{
+    id: 1,
+    name: 'Muhamad Gibran Al Mumbait',
+    email: 'muhamadgibran2333@example.com',
+    phone: "+628 5-842-xxx-xxx",
+}, ];
+
+
+module.exports = contacts;
